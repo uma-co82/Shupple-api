9d748f4efba1e42d81bf75b6917a1631663c94b1
磯崎 裕太 <isozakiyuta@LAB-N1533.local>
2019-08-10 16:53:38 +0900

    [fix] docker-compose_Volumes

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM golang:latest
 
 # ディレクトリ構成は今後変更の可能性あり
-COPY src/ /go/src/
+COPY src/api /go/src/api
 
-WORKDIR /go/src/
+WORKDIR /go/src/api/
 
 RUN go get -u github.com/gin-gonic/gin \
- && go get github.com/jinzhu/gorm
+  && go get github.com/jinzhu/gorm
 
 CMD ["go", "run", "main.go"]
--- src/api/main.go
+++ src/api/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"./db"
-	"./model"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	model.DBMigrate(db.DBConnect())
+	// model.DBMigrate(db.DBConnect())
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "ting",
+			"message": "ping",
 		})
 	})
 	r.Run(":8080")
