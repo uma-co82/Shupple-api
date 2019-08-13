842530c380c7168b1088030232d0198eb6260da4
磯崎 裕太 <isozakiyuta@LAB-N1533.local>
2019-08-13 21:39:50 +0900

    動作確認済

--- src/api/main.go
+++ src/api/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/holefillingco-ltd/Shupple-api/src/api/db"
+	"github.com/holefillingco-ltd/Shupple-api/src/api/server"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "ting",
-		})
-	})
 	db.Init()
-	r.Run(":8080")
+	server.Init()
 }
