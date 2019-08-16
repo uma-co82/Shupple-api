b7558bc54126b464b8446096b6db2bdabf51222c
磯崎 裕太 <isozakiyuta@LAB-N1533.local>
2019-08-16 20:50:36 +0900

    [add] BindJSONがどこまでやってくれるか試す

--- src/api/model/user.go
+++ src/api/model/user.go
@@ -1,14 +1,16 @@
 package model
 
 import (
+	"time"
+
 	"github.com/jinzhu/gorm"
 )
 
 type User struct {
 	gorm.Model
-	UID      string `json:"uid"`
-	NickName string `json:"nickName"`
-	Sex      string `json:"sex"`
-	Hobby    string `json:"hobby"`
-	BirthDay string `json:"birthDay"`
+	UID      string    `json:"uid"`
+	NickName string    `json:"nickName"`
+	Sex      string    `json:"sex"`
+	Hobby    string    `json:"hobby"`
+	BirthDay time.Time `json:"birthDay"`
 }
