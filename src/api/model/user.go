3db3f3e7b034f2c2a9b3bbb3ea1763609212535f
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2019-08-18 20:06:26 +0900

    モデル精査

--- src/api/model/user.go
+++ src/api/model/user.go
@@ -6,22 +6,47 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// DBへ保存する為の構造体
+// DBとのやり取りを担うUser構造体
 type User struct {
 	gorm.Model
-	UID      string    `json:"uid"`
-	NickName string    `json:"nickName"`
-	Sex      int       `json:"sex"`
-	Hobby    string    `json:"hobby"`
-	BirthDay time.Time `json:"birthDay"`
-	Age      int       `json:"Age"`
+	UID           string    `json:"uid"`
+	NickName      string    `json:"nickName"`
+	Sex           int       `json:"sex"`
+	BirthDay      time.Time `json:"birthDay"`
+	Age           int       `json:"Age"`
+	OpponentAge   int       `json:"opponentAge"`
+	ImageURL      string    `json:"imageUrl"`
+	InformationID int       `json:"informationID"`
+}
+
+// DBとのやり取りを担うUserInformation構造体
+type UserInformation struct {
+	gorm.Model
+	Hobby       string `json:"hobby"`
+	Residence   int    `json:"residence"`
+	Job         int    `json:"job"`
+	Personality int    `json:"personality"`
 }
 
 // POSTされた値を受け取る為の構造体
 type PostUser struct {
-	UID      string    `json:"uid"`
-	NickName string    `json:"nickName"`
-	Sex      int       `json:"sex"`
-	Hobby    string    `json:"hobby"`
-	BirthDay time.Time `json:"birthDay"`
+	UID         string    `json:"uid"`
+	NickName    string    `json:"nickName"`
+	Sex         int       `json:"sex"`
+	BirthDay    time.Time `json:"birthDay"`
+	OpponentAge int       `json:"opponentAge"`
+	Hobby       string    `json:"hobby"`
+	Residence   int       `json:"residence"`
+	Job         int       `json:"job"`
+	Personality int       `json:"personality"`
+}
+
+// アプリ側へ返却するUserのProfile構造体
+type Profile struct {
+	UID         string          `json:"uid"`
+	NickName    string          `json:"nickName"`
+	Sex         int             `json:"sex"`
+	Age         int             `json:"Age"`
+	OpponentAge int             `json:"opponentAge"`
+	Information UserInformation `json:"userInformation"`
 }
