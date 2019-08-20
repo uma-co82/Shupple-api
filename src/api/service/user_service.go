3dc8c624e26e8193ada16372f403359037872c44
磯崎 裕太 <isozakiyuta@LAB-N1533.local>
2019-08-20 19:46:20 +0900

    [fix] method切り出そうの会

--- src/api/service/user_service.go
+++ src/api/service/user_service.go
@@ -3,7 +3,6 @@ package service
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
 	"time"
 
 	"../db"
@@ -21,44 +20,6 @@ type Profile model.Profile
 
 type UID struct {
 	uid string `json:"uid"`
-}
-
-// レシーバーで受け取ったUserの異性を返す
-// 0 - 男性
-// 1 - 女性
-func (user *User) opponentSex() int {
-	switch user.Sex {
-	case 0:
-		return 1
-	case 1:
-		return 0
-	default:
-		return 0
-	}
-}
-
-// TODO: エラーハンドリング
-// 引数にとったTimeから年齢を算出
-func (user *User) calcAge(t time.Time) {
-	dateFormatOnlyNumber := "20060102" // YYYYMMDD
-
-	now := time.Now().Format(dateFormatOnlyNumber)
-	birthday := t.Format(dateFormatOnlyNumber)
-
-	// 日付文字列をそのまま数値化
-	nowInt, err := strconv.Atoi(now)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	birthdayInt, err := strconv.Atoi(birthday)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	age := (nowInt - birthdayInt) / 10000
-	user.Age = age
 }
 
 // ランダム取得
