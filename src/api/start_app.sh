b11072a763a43e3dcd05aa01bc6bc960dd36cff6
磯崎 裕太 <isozakiyuta@LAB-N1533.local>
2019-08-27 17:12:26 +0900

    [fix] start_app.sh &

--- src/api/start_app.sh
+++ src/api/start_app.sh
@@ -1,9 +1,11 @@
 # !/bin/bash
 
-echo 'waiting for mysqld to be connectable...'
 # MySQLサーバーが起動するまで待機する
 # TODO: mysqlサーバーの起動を検知するように書き換えるuntilとかで
-sleep 4
+until mysqladmin ping -h mysql -P 3306 --silent; do
+  echo 'waiting for mysqld to be connectable...'
+  sleep 2
+done
 
 echo "app is starting...!"
 exec go run main.go
