73f5b7c2b596c884d12d2babc0f5086a032ab09f
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-09-02 20:36:32 +0900

    [fix] minimum

--- src/api/service/error_method.go
+++ src/api/service/error_method.go
@@ -4,6 +4,6 @@ func (e *Error) Error() string {
 	return e.Message
 }
 
-func RaiseError(code int, msg string, vmsg []string) error {
-	return &Error{Code: code, Message: msg, ValidationMessage: vmsg}
+func RaiseError(code int, msg string, validationMessages []string) error {
+	return &Error{Code: code, Message: msg, ValidationMessage: validationMessages}
 }
