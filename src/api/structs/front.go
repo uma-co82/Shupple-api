9aabc8d591858afbc69d134203d4048b539f80c9
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-09-27 20:35:49 +0900

    Update front.go

--- src/api/structs/front.go
+++ src/api/structs/front.go
@@ -58,4 +58,5 @@ type Error struct {
 	Code              int      `json:"code"`
 	Message           string   `json:"message"`
 	ValidationMessage []string `json:"validationMessage"`
+	FrontMessage      string   `json:"frontMessage"`
 }
