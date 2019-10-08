42cfce68afb2210ddc8b88e31492573c5337b9a8
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-10-08 18:03:18 +0900

    Update db.go

--- src/api/db/db.go
+++ src/api/db/db.go
@@ -1,6 +1,3 @@
-/***********************************************************
- *                        local                            *
- ***********************************************************/
 package db
 
 import (
@@ -45,41 +42,3 @@ func AutoMigration() {
 	db.AutoMigrate(&structs.UserCombination{})
 	db.AutoMigrate(&structs.InfoCompatible{})
 }
-
-/***********************************************************
- *                    Production                           *
- ***********************************************************/
-//package db
-//
-//import (
-////"../structs"
-//"github.com/jinzhu/gorm"
-//_ "github.com/jinzhu/gorm/dialects/mysql"
-//"github.com/uma-co82/Shupple-api/src/api/structs"
-//)
-//
-//var (
-//	db  *gorm.DB
-//	err error
-//)
-//
-//func Init() *gorm.DB {
-//	DBMS := "mysql"
-//	USER := "shupple"
-//	PASS := "shupple1995"
-//	PROTOCOL := "tcp(shupple-api-db.cniwd3cd12wv.ap-northeast-1.rds.amazonaws.com:3306)"
-//	DBNAME := "shupple"
-//	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
-//	db, err = gorm.Open(DBMS, CONNECT)
-//	if err != nil {
-//		panic(err.Error())
-//	}
-//	return db
-//}
-//
-//func AutoMigration() {
-//	db.AutoMigrate(&structs.User{})
-//	db.AutoMigrate(&structs.UserInformation{})
-//	db.AutoMigrate(&structs.UserCombination{})
-//	db.AutoMigrate(&structs.InfoCompatible{})
-//}
