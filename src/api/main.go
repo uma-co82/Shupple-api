1f4a2e0817e5f02a6703418acf337e09ef9168a7
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-10-28 22:48:02 +0900

    cron動かん問題

--- src/api/main.go
+++ src/api/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/carlescere/scheduler"
 	"github.com/gin-gonic/gin"
 	"github.com/uma-co82/Shupple-api/src/api/db"
 	"github.com/uma-co82/Shupple-api/src/api/server"
-	"github.com/uma-co82/Shupple-api/src/api/task"
 )
 
 func main() {
@@ -14,5 +12,5 @@ func main() {
 	server.Init()
 	gin.SetMode(gin.ReleaseMode)
 
-	scheduler.Every(1).Minutes().Run(task.UserCombinationCheckCreatedAtTask)
+	//scheduler.Every(1).Minutes().Run(task.UserCombinationCheckCreatedAtTask)
 }
