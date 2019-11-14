dd6a49776c693d3860ee04499db12d2d80a4187c
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-11-14 20:33:11 +0900

    [fix] tmp_comment_out

--- src/api/main.go
+++ src/api/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"github.com/carlescere/scheduler"
 	"github.com/uma-co82/Shupple-api/src/api/infrastructure/db"
 	"github.com/uma-co82/Shupple-api/src/api/infrastructure/server"
-	"github.com/uma-co82/Shupple-api/src/api/task"
 )
 
 func main() {
 	db.Init()
 	db.AutoMigration()
-	go func() {
-		scheduler.Every(1).Minutes().Run(task.UserCombinationCheckCreatedAtTask)
-	}()
+	//go func() {
+	//	scheduler.Every(1).Minutes().Run(task.UserCombinationCheckCreatedAtTask)
+	//}()
 	server.Init()
 }
